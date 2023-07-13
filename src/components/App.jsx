2d4cba9d54c9fb435a70cbe0227ef547d2ffe9e7
Denys Tryfonov <trifonowden123@gmail.com>
2023-07-13 17:19:53 +0300

    add loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework templateq
-    </div>
-  );
-};
+import React, { Component } from "react";
+import axios from "axios";
+import { Loader } from "./Loader/Loader";
+
+axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
+
+const ArticleList = ({ articles }) => (
+  <ul>
+    {articles.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+export class App extends Component {
+  state = {
+    articles: [],
+    isLoading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ isLoading: true })
+    const response = await axios.get("/search?query=react");
+    this.setState({
+      articles: response.data.hits,
+      isLoading: false,
+    });
+  }
+
+  render() {
+    const { articles, isLoading } = this.state;
+    return (
+      <div>
+        {isLoading ? <Loader/> : <ArticleList articles={articles} />}
+      </div>
+    );
+  }
+}
